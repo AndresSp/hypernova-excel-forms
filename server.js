82939f8923519e6f087e6430695f864e3b6c9f12
AndresSp <andrestunonsp@gmail.com>
2021-02-21 21:46:30 -0500

    Implement Material UI

--- server.js
+++ server.js
@@ -1,65 +1,41 @@
 'use strict';
 
 const Hapi = require('@hapi/hapi');
-const Vision = require('@hapi/vision');
-const HapiReactViews = require('hapi-react-views');
-const Inert = require('@hapi/inert');
-const path = require('path');
-
+const next = require('next')
+const { pathWrapper, nextHandlerWrapper } = require('./next-wrapper')
 require('dotenv').config();
 
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
 
-require('@babel/register')({
-    presets: ['@babel/preset-react', '@babel/preset-env']
+const server = Hapi.Server({
+    host: 'localhost',
+    port: parseInt(process.env.PORT, 10) || 3000
 });
-
 
 const main = async function () {
 
-    const server = Hapi.Server({
-        host: 'localhost',
-        port: process.env.PORT || 3000
-    });
-
-    await server.register(Vision);
-    await server.register(Inert);
-
-    server.views({
-        engines: {
-            jsx: HapiReactViews
-        },
-        relativeTo: __dirname,
-        path: 'pages',
-        compileOptions: {
-            renderMethod: 'renderToString',
-            layoutPath: path.join(__dirname, 'pages'),
-            layout: 'layout'
-        }
-    });
-
-    server.route({
+    await app.prepare()
+    
+      server.route({
         method: 'GET',
-        path: '/assets/client.js',
-        handler: {
-            file: path.join(__dirname, './public/client.js')
-        }
-    });
+        path: '/_next/{p*}' /* next specific routes */,
+        handler: nextHandlerWrapper(app),
+      })
+    
+      server.route({
+        method: '*',
+        path: '/{p*}' /* catch all route */,
+        handler: nextHandlerWrapper(app),
+      })
 
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-
-            const context = { title: 'Formulario para Pago de Horas Extras - Hypernova Labs' };
-            context.state = `window.state = ${JSON.stringify(context)};`;
-
-            return h.view('app', context);
-        }
-    });
-
-    await server.start();
-
-    console.log(`Server is listening at ${server.info.uri}`);
+    try {
+        await server.start();
+        console.log(`Server is listening at ${server.info.uri}`);
+    } catch (error) {
+        console.log('Error starting server')
+        console.log(error)
+    }
 };
 
 main();
