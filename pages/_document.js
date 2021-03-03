fff1af981817d4e9b5dd241d58ff9dd726cac3fa
AndresSp <andrestunonsp@gmail.com>
2021-03-03 02:19:27 -0500

    fix font

--- pages/_document.js
+++ pages/_document.js
@@ -11,9 +11,9 @@ export default class MyDocument extends Document {
           {/* PWA primary color */}
           <meta name='theme-color' content={theme.palette.primary.main} />
           <link
-            rel='preload'
-            href='/fonts/Gotham.css' 
-            as='style'
+            rel='stylesheet'
+            href='/fonts/Gotham.css'
+            
           />
           <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png"/>
           <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png"/>
